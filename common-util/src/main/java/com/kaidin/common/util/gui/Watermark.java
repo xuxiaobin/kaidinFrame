d110a704fa16f0130ca17d0474c7fd44efa1c432
xuxiaobin <xuxiaobin@4965036e-fb9b-4576-aac7-8f1c413749bf>
2016-04-29 01:41:35 +0000

    去掉多余的包引用

--- common-util/src/main/java/com/kaidin/common/util/gui/Watermark.java
+++ common-util/src/main/java/com/kaidin/common/util/gui/Watermark.java
@@ -3,7 +3,6 @@ package com.kaidin.common.util.gui;
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
